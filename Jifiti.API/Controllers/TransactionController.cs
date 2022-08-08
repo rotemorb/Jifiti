609ff2a9bd3dad1cf7125177894498eaff392f97
Rotem Orbach <orbachrotem@gmail.com>
2022-08-08 11:02:41 +0300

    Add project files.

---
+++ Jifiti.API/Controllers/TransactionController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jifiti.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionsController : ControllerBase
+    {
+
+    }
+}
---
+++ Jifiti.API/Person.cs
@@ -0,0 +1,12 @@
+namespace Jifiti.API
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+    }
+}
